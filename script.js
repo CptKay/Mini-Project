7f2892df516ccde97ccaa334b6589c5ddd82f0e4
‘CptKay’ <sammerz@bluewin.ch>
2022-12-21 11:10:48 +0100

    dev branch

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/* Change mood */
+
+function smile() {
+    let emojiObject = document.getElementById("emoji");
+    emojiObject = "bi bi-emoji-heart-eyes";
+
+}
+
+function frown() {
+    let emojiObject = document.getElementById("emoji");
+    emojiObject = "bi bi-emoji-heart-frown";
+}
+
+
